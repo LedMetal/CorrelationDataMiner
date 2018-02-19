8e1b0946eed0efb49f19600005adb137675f6a0b
Abdul Sadiq <abdulsadiq@hotmail.com>
2018-02-18 22:14:20 -0500

    Create Interval class

---
+++ CorrelationDataMiner/bus/Interval.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrelationDataMiner.bus
+{
+    public class Interval
+    {
+        int firstPosition, lastPosition, intervalLength;
+
+        public Interval()
+        {
+            this.firstPosition = 0;
+            this.lastPosition = 0;
+            this.intervalLength = 0;
+        }
+
+        // Calculates the length of the interval
+        public void CalculateIntervalLength()
+        {
+            this.intervalLength = (this.lastPosition + 1) - this.firstPosition;
+        }
+
+        /*--------------------------------------------------------------------------------------------*/
+
+        public int FirstPosition { get => firstPosition; set => firstPosition = value; }
+        public int LastPosition { get => lastPosition; set => lastPosition = value; }
+        public int IntervalLength { get => intervalLength; set => intervalLength = value; }
+    }
+
+}
