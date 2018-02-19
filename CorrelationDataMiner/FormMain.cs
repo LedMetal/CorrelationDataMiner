efe11bc9b44e879fb02816e1528c21df1b4b621c
Abdul Sadiq <abdulsadiq@hotmail.com>
2018-02-18 23:57:04 -0500

    Create method to read signal one file and store appropriately into frame object

--- CorrelationDataMiner/FormMain.cs
+++ CorrelationDataMiner/FormMain.cs
@@ -73,11 +73,6 @@ namespace CorrelationDataMiner
             }
         }
 
-        private void btnCalculateIntervals_Click(object sender, EventArgs e)
-        {
-            ReadFiles();
-        }
-
         private void btnBrowseSig2_Click(object sender, EventArgs e)
         {
             // Create OpenFileDialog object to select signal two file
@@ -99,12 +94,19 @@ namespace CorrelationDataMiner
             }
         }
 
+        private void btnCalculateIntervals_Click(object sender, EventArgs e)
+        {
+            ReadFiles();
+        }
+
         // Read and store files into global list of Frame objects
         private void ReadFiles()
         {
             ReadCorrelationFile();
+            ReadSignalOneFile();
         }
 
+        // Read correlation signal file and store into newly created Frame object
         private void ReadCorrelationFile()
         {
             // Open StreamReader object for correlation file
@@ -121,6 +123,27 @@ namespace CorrelationDataMiner
 
                     // Add new frame object to global list, saving the frame's position and correlation signal
                     framesList.Add(new Frame() { Position = position, CorrSignal = Convert.ToDouble(line) });
+                }
+            }
+        }
+
+        // Read signal one file and store into frame object at appropriate position
+        private void ReadSignalOneFile()
+        {
+            // Open StreamReader object for signal one file
+            using (StreamReader reader = new StreamReader(signalOneFile))
+            {
+                string line = "";
+                int position = 0;
+
+                // Loop through all lines that are not read as null
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Use position variable as index to find corresponding frame and store signal one value
+                    framesList[position].SigOne = Convert.ToDouble(line);
+
+                    // Increment position variable
+                    position++;
                 }
             }
         }
